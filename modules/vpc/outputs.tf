b0c2f360986b6dfabe1b24b41e8ce151fef1b8a3
Qandeel Javed <qandeel.javed@chasacademy.se>
2026-04-26 13:48:10 +0200

    fix: add second private subnet ID to VPC outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -17,3 +17,8 @@ output "private_subnet_id" {
   description = "ID of the private subnet"
   value       = aws_subnet.private.id
 }
+
+output "private_subnet_b_id" {
+  description = "ID of the second private subnet"
+  value       = aws_subnet.private_b.id
+}
